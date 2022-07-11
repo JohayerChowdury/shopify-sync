377a3ac932a1a87a95b32e163f00b0f8f4a1a4ee
Mathew <nacevm@mcmaster.ca>
2022-07-11 16:22:34 -0400

    Allows for log in form and preliminary posting

--- shopfiy-sync-frontend/src/App.js
+++ shopfiy-sync-frontend/src/App.js
@@ -1,8 +1,61 @@
 // import logo from './logo.svg';
 import './App.css';
+import {createContext , useState, useEffect} from 'react';
+import axios from 'axios';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Login from './components/Login';
+import NavigationBar from "./components/NavigationBar";
+
+
+export const UserContext = createContext();
+
 
 function App() {
-  return <h1>Placeholder text for frontend</h1>;
+
+  const [userData , setUserData] = useState({
+    token: undefined,
+    user: undefined
+  })
+
+  useEffect(() => {
+    const isLoggedIn = async() => {
+      let token = localStorage.getItem("auth-token")
+      if(token == null) {
+        localStorage.setItem("auth-token", "")
+        token = ""
+      }
+      const tokenResponse = await axios.post(
+        '/shopify_api/users/tokenIsValid', null, {headers: {"auth-token": token}}
+      )
+      console.log(tokenResponse.data)
+      if(tokenResponse.data) {
+        const userResponse = await axios.get('api/users/profile', {headers: {'auth-tokn': token}})
+        setUserData({
+          token: token,
+          user: userResponse.data
+        })
+      }
+    }
+    isLoggedIn()
+  }, [])
+
+
+  return(
+    <BrowserRouter>
+    <UserContext.Provider value = {{userData, setUserData}}>
+    <div class = "justify-content-center">
+      <NavigationBar />
+    </div>
+    <Routes>
+      <Route path = "/" element = {<Home />} />
+      <Route path = '/login' element = {<Login />} />
+    </Routes>
+    </UserContext.Provider>
+    </BrowserRouter>
+
+  );
+
 }
 
 export default App;
